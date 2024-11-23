b595e52cf0ae9ed995c36b1e626f7fc019b9a660
Fedir Kryvytskyi <krivfedor@gmail.com>
2024-11-23 14:21:13 +0100

    Minor refactor

--- src/widgets/widgets.zig
+++ src/widgets/widgets.zig
@@ -12,17 +12,7 @@ pub fn runApp() !void {
     gtk.gtk_init();
 
     registerTypes();
-
-    const b = gtk.gtk_builder_new() orelse return AppError.InitializationFailed;
-    // Construct a GtkBuilder instance and load our UI description
-    var err: [*c]gtk.GError = null;
-    if (gtk.gtk_builder_add_from_string(b, builder_ui, builder_ui.len, &err) == 0) {
-        gtk.g_printerr("Error loading file: %s\n", err.*.message);
-        gtk.g_clear_error(&err);
-        return AppError.InitializationFailed;
-    }
-
-    builder = b;
+    try initBuilder();
 
     const app = gtk.gtk_application_new("org.gtk.example", gtk.G_APPLICATION_DEFAULT_FLAGS);
     _ = gtk.signalConnect(app, "activate", @ptrCast(&activate), null);
@@ -36,7 +26,20 @@ fn registerTypes() void {
     _ = gtk.registerType(example_widget.ExampleWidget, example_widget.ExampleWidgetClass);
 }
 
-pub fn activate(app: *gtk.GtkWidget, _: gtk.gpointer) void {
+fn initBuilder() !void {
+    const b = gtk.gtk_builder_new() orelse return AppError.InitializationFailed;
+    // Construct a GtkBuilder instance and load our UI description
+    var err: [*c]gtk.GError = null;
+    if (gtk.gtk_builder_add_from_string(b, builder_ui, builder_ui.len, &err) == 0) {
+        gtk.g_printerr("Error loading file: %s\n", err.*.message);
+        gtk.g_clear_error(&err);
+        return AppError.InitializationFailed;
+    }
+
+    builder = b;
+}
+
+fn activate(app: *gtk.GtkWidget, _: gtk.gpointer) void {
     // Connect signal handlers to the constructed widgets.
     const window = gtk.gtk_builder_get_object(builder orelse return, "window");
     gtk.gtk_window_set_application(@ptrCast(window), @ptrCast(app));
